7deb226d2e4c9f6c80922198ae706c3c757f54a9
Ocotiio <57273707+Ocotiio@users.noreply.github.com>
2019-11-24 22:33:12 -0800

    cleanup
    cleanup

--- computercraft_dir/miners/startup_http.lua
+++
@@ -1,7 +0,0 @@
--- startup_http
-
--- Turtle startup, call http, download something
-
-local sExample = http.get("http://example.com/") --Get contents of page
-write(sExample.readAll()) --Read and print contents of page
-sExample.close() --Just in case
